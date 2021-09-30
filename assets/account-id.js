2487fac185d20b38d62a141bdf74eead1fe31488
Artlfmj <55346663+Artforlfmj@users.noreply.github.com>
2021-09-30 09:47:34 +0200

    Add initial request + platforms

--- assets/account-id.js
+++ assets/account-id.js
@@ -1,11 +1,26 @@
 const axios = require('axios')
 const readline = require('readline')
 const chalk = require('chalk')
+const endpoints = require('./endpoints.json')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 rl.question('What is your account name?', async(answer) => {
-  console.log(chalk.red(`Fetching account linked or named ${answer}`))
+  console.log(chalk.red(`Fetching account linked or named ${answer}`));
+  const platforms = ["epic","psn", "xbl"] 
+  for(const platform of platforms){
+    axios({
+      url : endpoints.fnapicom.accountid,
+      method : "get",
+      params : {
+        name : answer.toString(),
+        platform : platform
+      },
+      headers : {
+        'x-api-key' : process.env.FNAPICOM
+      }
+    })
+   }
 })
